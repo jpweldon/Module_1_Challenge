97eb4af39475d9fb5a9a05d173b50a4e08a30442
John P Weldon <johnpweldon@Johns-MacBook-Air.local>
2021-06-23 18:45:16 -0500

    Defined functions and calculated total, count, and average of loans.

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -17,17 +17,27 @@ loan_costs = [500, 600, 200, 1000, 450]
 # How many loans are in the list?
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
-# YOUR CODE HERE!
+def loan_count(loan_info):
+    number_of_loans = len(loan_info)
+    print(f"There is a total of {number_of_loans} loans.")
+loan_count(loan_costs)
+
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
-# YOUR CODE HERE!
+def loan_total(loan_data):
+    total_of_loans = sum(loan_data)
+    print(f"The loans sum to a total of ${total_of_loans: .2f}.")
+loan_total(loan_costs)
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
-# YOUR CODE HERE!
+def loan_average (loan_material):
+    average_of_loans = sum(loan_material)/len(loan_material)
+    print(f"The average loan price is ${average_of_loans: .2f}.")
+loan_average(loan_costs)
 
 """Part 2: Analyze Loan Data.
 
@@ -107,7 +117,7 @@ new_loan = {
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+# commenting for testing: print(f"The present value of the loan is: {present_value}")
 
 
 """Part 4: Conditionally filter lists of loans.
